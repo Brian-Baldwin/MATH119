a68d0da6f35758ec85fba34862ac7f8d85322df6
Brian-Baldwin <97992446+Brian-Baldwin@users.noreply.github.com>
2022-02-14 20:55:45 -0500

    Update newtons_method.py

--- newtons_method.py
+++ newtons_method.py
@@ -1,13 +1,17 @@
 import numexpr as ne
-x = float(input("Enter X0: "))
-func = input("Enter the function: ")
-derivative = input("Enter the function's derivative: ")
-approxNum = int(input("n = ?: "))
-for i in range(approxNum):
-    evalFunc = float(ne.evaluate(func))
-    evalDeriv = float(ne.evaluate(derivative))
-    x = x - evalFunc / evalDeriv
 
 
-print(x)
+def newtons_method():
+    x = float(input("Enter X0: "))
+    func = input("Enter the function: ")
+    derivative = input("Enter the function's derivative: ")
+    approxNum = int(input("n = ?: "))
+    for i in range(approxNum):
+        evalFunc = float(ne.evaluate(func))
+        evalDeriv = float(ne.evaluate(derivative))
+        x = x - evalFunc / evalDeriv
+    return x
 
+
+if __name__ == "__main__":
+    print(newtons_method())
